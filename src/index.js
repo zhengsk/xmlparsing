9a71285e6a645b1d6aaaf372d8ed5e5a2235d0a2
大二 <daer.zsk@antfin.com>
2018-08-17 21:51:31 +0800

    Nothing

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: daer
+ * @Date: 2018-08-17 21:40:03
+ * @Last Modified by: daer
+ * @Last Modified time: 2018-08-17 21:41:42
+ */
+
+import parser from './parser';
+
+parser('abc');
