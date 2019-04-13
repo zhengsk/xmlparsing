94ee984a02846dfa1ad26bcb37e2b91370b21274
zhengsk <zsk526@qq.com>
2019-04-13 10:10:48 +0800

    chore: nothing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
-import parse, { AST } from './parser';
+import parser, { AST } from './parser';
 import generator from './generator';
 
 const xmlStr: string = fs.readFileSync('./src/template', 'utf8');
 
-const ast: AST = parse.parse(xmlStr);
+const ast: AST = parser.parse(xmlStr);
 console.info(ast);
 
 const newXmlStr: string = generator.generate(ast, { format: true });
