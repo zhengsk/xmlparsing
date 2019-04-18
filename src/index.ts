8d7bd547f33008f76a36c8b16942cf05cd20b9f2
zhengsk <zsk526@qq.com>
2019-04-18 11:28:44 +0800

    fix: export parser

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { Document } from './node';
 import parser from './parser';
 import generator from './generator';
 
-const xmlStr: string = fs.readFileSync('./src/template', 'utf8');
+// const xmlStr: string = fs.readFileSync('./src/template', 'utf8');
+// const ast: Document = parser.parse(xmlStr);
+// const newXmlStr: string = generator.generate(ast, { format: true });
 
-const ast: Document = parser.parse(xmlStr);
-console.info(ast);
-
-const newXmlStr: string = generator.generate(ast, { format: true });
-
-debugger; // tslint:disable-line
+export default {
+  parser,
+  generator
+};
